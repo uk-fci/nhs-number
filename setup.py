3b48b3e100fa287ed49ca58749834614d4346ad7
Andy Law <andy.law@roslin.ed.ac.uk>
2021-02-21 19:38:16 +0000

    Updated code status classifier

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
         'Topic :: Office/Business :: Financial',
         'Topic :: Scientific/Engineering :: Mathematics',
