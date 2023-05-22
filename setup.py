a4c5dafda1db1f416dc57d104ea2aad17d1b99c9
Andy Law <andy.law@roslin.ed.ac.uk>
2023-05-22 16:12:26 +0100

    Bumped the version number in the setup files

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.2.1'
+VERSION = '1.2.2'
 
 setup(
     name='nhs_number',
@@ -30,5 +30,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ]
 )
