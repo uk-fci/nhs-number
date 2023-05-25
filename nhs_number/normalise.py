803acbaa4e3bbf8e64eaacdcf6e3ae0118cbd311
Marcus Baw <marcusbaw@gmail.com>
2023-05-25 08:44:10 +0100

    adds type hints and conversion to string (just in case)

--- nhs_number/normalise.py
+++ nhs_number/normalise.py
@@ -6,10 +6,10 @@ import re
 
 GOOD_FORMAT = r'^(\d{10}|\d{3} \d{3} \d{4}|\d{3}-\d{3}-\d{4})$'
 
-def normalise_number(nhs_number):
+def normalise_number(nhs_number:str) -> str:
     """
     Extract the 10 digits of an NHS number if the supplied string is a valid
-    format.
+    format. If supplied as an int it will attempt to convert it to a string for processing.
     Valid formats are:
       123 456 7890
       123-456-7890
@@ -22,7 +22,7 @@ def normalise_number(nhs_number):
     :param nhs_number:
     :return: 10 digit numerical string or the empty string
     """
-    working_number = nhs_number.strip()
+    working_number = str(nhs_number).strip()
     if re.search(GOOD_FORMAT, working_number) is None:
         working_number = ''
     working_number = re.sub('[- ]', '', working_number)
