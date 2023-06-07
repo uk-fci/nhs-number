5148df101c19633447724cc32217e0d7839c2d4a
Marcus Baw <marcusbaw@gmail.com>
2023-06-07 12:38:18 +0100

    refactors the check digit calculation into its own function to allow reuse

--- nhs_number/validate.py
+++ nhs_number/validate.py
@@ -1,19 +1,51 @@
 """
 Validates NHS Numbers
-"""
-# License: MIT (http://www.opensource.org/licenses/mit-license.php)
 
-__version__ = '1.2.1'
-__author__ = "Andy Law <andy.law@roslin.ed.ac.uk>"
+Originally derived from Andy Law's nhs_number Python package validation code
+
+License: MIT (http://www.opensource.org/licenses/mit-license.php)
+
+Contributors
+* Andy Law <andy.law@roslin.ed.ac.uk>
+* Marcus Baw <marcusbaw@gmail.com>
+"""
 
 from nhs_number.normalise import normalise_number
 
-def is_valid(nhs_number:str) -> bool:
+
+def calculate_checksum(identifier_digits: str) -> int:
+    """
+    Calculates the checksum digit for the supplied NHS number (as a string) and returns it as an integer.
+    IMPORTANT: Expects a string of NINE digits (ie REMOVE the check digit, don't supply a full NHS number)
+    """
+    if len(identifier_digits) != 9:
+        return None
+
+    # For the algorithm that calculates the checksum digit, see
+    # https://www.closer.ac.uk/wp-content/uploads/CLOSER-NHS-ID-Resource-Report-Apr2018.pdf
+    parts_list = [
+        int(digit) * (10 - index) for index, digit in enumerate(identifier_digits)
+    ]
+    list_sum = sum(parts_list)
+    checksum = 11 - (list_sum % 11)
+    if checksum == 11:
+        checksum = 0
+    return checksum
+
+
+def is_valid(nhs_number: str) -> bool:
     """
     Checks the supplied NHS number (as a string) is valid and returns True or False.
 
     Internally this uses the normalise_number() function to check that this is a valid format to start with.
     Any non-valid input will result in returning False.
+
+    The NHS number must:
+        1. be 10 digits long
+        2. The 10th digit is a check digit to confirm validity using the modulus 11 method
+
+    How NHS Number validation works: https://www.datadictionary.nhs.uk/attributes/nhs_number.html
+
     """
 
     nhs_number = normalise_number(nhs_number)
@@ -21,16 +53,10 @@ def is_valid(nhs_number:str) -> bool:
         return False
 
     # The first 9 numbers are used to calculate the checksum, which should match the last digit
-    (first_part, check_digit) = (nhs_number[:-1], int(nhs_number[-1]))
+    (identifier_digits, check_digit) = (nhs_number[:-1], int(nhs_number[-1]))
 
-    # For the algorithm that calculates the checksum digit, see
-    # https://www.closer.ac.uk/wp-content/uploads/CLOSER-NHS-ID-Resource-Report-Apr2018.pdf
-    parts_list = [int(digit) * (10 - index) for index, digit in enumerate(first_part)]
-    list_sum = sum(parts_list)
-    checksum = 11 - (list_sum % 11)
-    if checksum == 11:
-        checksum = 0
+    calculated_checksum = calculate_checksum(identifier_digits)
 
     # NOTE: a checksum of 10 is invalid (and is quoted as a special case), but the check for equality
     # will catch that circumstance anyway
-    return False if checksum != check_digit else True
+    return False if calculated_checksum != check_digit else True
