6af66f1a074de8be979c908bc706cdf5b4b7720a
Marcus Baw <marcusbaw@gmail.com>
2023-06-07 15:35:21 +0100

    tidying and docstrings

--- nhs_number/__init__.py
+++ nhs_number/__init__.py
@@ -1,12 +1,17 @@
 """
 __init__.py
+
+Explicit imports into __init__.py are used in order to allow separate files without
+affecting the namespacing of the original library.
 """
 
-# explicit imports into __init__.py are used in order to allow separate files
-# without affecting the namespacing of the original library.
-from nhs_number.constants import REGIONS
+# standard imports
+
+# third-party imports
+
+# local imports
+from nhs_number.constants import REGIONS, Region
 from nhs_number.details import NhsNumber
 from nhs_number.generate import generate
 from nhs_number.normalise import normalise_number
 from nhs_number.validate import is_valid, calculate_checksum
-
