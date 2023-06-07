b21c12d9107035aa2f005d34d94427adcfec9fce
Marcus Baw <marcusbaw@gmail.com>
2023-06-07 12:38:34 +0100

    init.py imports

--- nhs_number/__init__.py
+++ nhs_number/__init__.py
@@ -4,8 +4,9 @@ __init__.py
 
 # explicit imports into __init__.py are used in order to allow separate files
 # without affecting the namespacing of the original library.
-from nhs_number.validate import is_valid
+from nhs_number.constants import REGIONS
+from nhs_number.details import NhsNumber
+from nhs_number.generate import generate
 from nhs_number.normalise import normalise_number
-
-
+from nhs_number.validate import is_valid, calculate_checksum
 
