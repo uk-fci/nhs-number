55daea915d671330b95c76f24f5255ba34136926
Andy Law <andy.law@roslin.ed.ac.uk>
2023-06-10 15:30:43 +0100

    Reformat __init__.py to match PEP8 (Issue #13)

--- nhs_number/__init__.py
+++ nhs_number/__init__.py
@@ -1,8 +1,8 @@
 """
 __init__.py
 
-Explicit imports into __init__.py are used in order to allow separate files without
-affecting the namespacing of the original library.
+Explicit imports into __init__.py are used in order to allow separate files
+without affecting the namespacing of the original library.
 """
 
 # standard imports
@@ -11,9 +11,18 @@ affecting the namespacing of the original library.
 
 # local imports
 from nhs_number.constants import REGIONS, Region
-from nhs_number.constants import REGION_UNALLOCATED, REGION_RESERVED, \
-    REGION_ENGLAND, REGION_WALES, REGION_IOM, REGION_ENGLAND_WALES_IOM, \
-    REGION_SCOTLAND, REGION_NORTHERN_IRELAND, REGION_SYNTHETIC, REGION_EIRE
+from nhs_number.constants import (
+    REGION_UNALLOCATED,
+    REGION_RESERVED,
+    REGION_ENGLAND,
+    REGION_WALES,
+    REGION_IOM,
+    REGION_ENGLAND_WALES_IOM,
+    REGION_SCOTLAND,
+    REGION_NORTHERN_IRELAND,
+    REGION_SYNTHETIC,
+    REGION_EIRE,
+)
 from nhs_number.constants import FULL_RANGE
 from nhs_number.details import NhsNumber
 from nhs_number.generate import generate
